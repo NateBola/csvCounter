23064f6d1e45d788242bd4081694d53349ffa8f9
Neythan <neythanpoling@gmail.com>
2024-09-13 13:29:14 -0400

    Revert "Removed opening section from Snap"
    This reverts commit bf24f3b99b4533596db735956d799592324eabf7.

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ int main(){
         printf("%s", filePath);
     #endif
 
-    /*
     record = fopen(filePath, "a");
     while (count < 10){
         count += 1;
@@ -31,9 +30,7 @@ int main(){
             printf("Update Entry\n");
         #endif
     }
-    */
     return 0;
-    
 }
 
 
