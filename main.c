2938ab66329307725318f9a302717f108b256d28
Neythan <neythanpoling@gmail.com>
2024-09-13 13:55:59 -0400

    01 Trying new path

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(){
         filePath = "log.csv";
     #endif
     #ifdef __linux__
-        filePath = "/home/ubuntu/log/log.csv";
+        filePath = "/home/ubuntu/snap/csvcounter/common/log.csv";
     #endif
 
     record = fopen(filePath, "a");
