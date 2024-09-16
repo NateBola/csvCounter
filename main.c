7c7a0d6fefe0aec0b8844ad7884bc3241a24daca
Neythan <neythanpoling@gmail.com>
2024-09-16 09:23:32 -0400

    New changes to path using env variables

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <timefunctions.h>
+
+#undef FILE_PRINT 0
 
 int main(){
     uint64_t count = 0;
@@ -11,18 +15,30 @@ int main(){
         filePath = "log.csv";
     #endif
     #ifdef __linux__
-        filePath = "/var/snap/csvcounter/common/log.csv";
+        char *fileRoot = getenv("SNAP_USER_DATA");
+        if (val == NULL)
+        {
+            printf("Cannot find enviroment variable");
+        }
+        else:
+            printf("%s\n", fileRoot);
+            strcat(filePath, fileRoot);
+            printf("%s\n", filePath);
+            strcat(filePath. "/log.csv");
+            printf("%s\n", filePath);
     #endif
 
-    record = fopen(filePath, "a");
-    while (count < 10){
-        count += 1;
-        fprintf(record, "Entering Log, %d\n", count);
-        scriptSleep(1000);
-        #ifdef _WIN32
-            printf("Update Entry\n");
-        #endif
-    }
+    #ifdef FILE_PRINT
+        record = fopen(filePath, "a");
+        while (count < 10){
+            count += 1;
+            fprintf(record, "Entering Log, %d\n", count);
+            scriptSleep(1000);
+            #ifdef _WIN32
+                printf("Update Entry\n");
+            #endif
+        }
+    #endif
     return 0;
 }
 
